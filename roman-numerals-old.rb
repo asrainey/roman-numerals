b0459270687b88fe490b8e76b428a8afbf877ebd
Allison Rainey <allisonsrainey@gmail.com>
2017-04-04 18:56:28 -0400

    Condensed into 1 if statement (and through 99)

--- roman-numerals-old.rb
+++ roman-numerals-old.rb
@@ -1,15 +1,7 @@
 #write a method to convert an integer to a Roman numeral string
 def convert number
-  if number.to_i <= 4
-    old_roman_numeral = 'I' * number.to_i
-
-  elsif number.to_i <= 9
-    old_roman_numeral = 'V' + ('I' * (number.to_i - 5)).to_s
-
-  elsif number.to_i <= 49
-    old_roman_numeral = ('X' * (number.to_i / 10).to_i).to_s + ('V' * ((number.to_i%10).to_i / 5).to_i).to_s + ('I' * (number.to_i%5).to_i).to_s
-
-  elsif number.to_i <= 99
+  if number.to_i <= 99
+    old_roman_numeral = ('L' * (number.to_i / 50).to_i).to_s + ('X' * ((number.to_i%50).to_i / 10).to_i).to_s + ('V' * ((number.to_i%10).to_i / 5).to_i).to_s + ('I' * (number.to_i%5).to_i).to_s
 
   elsif number.to_i <= 499
 
