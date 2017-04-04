846e657b1dfd6d67cbc570e9c999df70ba9f0152
Allison Rainey <allisonsrainey@gmail.com>
2017-04-04 19:12:08 -0400

    Working program

--- roman-numerals-old.rb
+++ roman-numerals-old.rb
@@ -1,15 +1,9 @@
 #write a method to convert an integer to a Roman numeral string
 def convert number
-  if number.to_i <= 99
-    old_roman_numeral = ('L' * (number.to_i / 50).to_i).to_s + ('X' * ((number.to_i%50).to_i / 10).to_i).to_s + ('V' * ((number.to_i%10).to_i / 5).to_i).to_s + ('I' * (number.to_i%5).to_i).to_s
-
-  elsif number.to_i <= 499
-
-  elsif number.to_i <= 999
-
+  if number.to_i <= 3000
+    old_roman_numeral = ('M' * (number.to_i / 1000).to_i).to_s + ('D' * ((number.to_i%1000).to_i / 500).to_i).to_s + ('C' * ((number.to_i%500).to_i / 100).to_i).to_s + ('L' * ((number.to_i%100) / 50).to_i).to_s + ('X' * ((number.to_i%50).to_i / 10).to_i).to_s + ('V' * ((number.to_i%10).to_i / 5).to_i).to_s + ('I' * (number.to_i%5).to_i).to_s
   else
-    #numbers >= 1000
-
+    puts 'That won\'t work. Please pick a number between 1 and 3000.'
   end
   puts old_roman_numeral
 end
