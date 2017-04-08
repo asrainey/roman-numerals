aff7772bcbd11ee06aa4d259fe5c10b3a15d8750
Allison Rainey <allisonsrainey@gmail.com>
2017-04-08 14:11:36 -0400

    Shortened program

--- roman-numerals-old.rb
+++ roman-numerals-old.rb
@@ -2,8 +2,6 @@
 def convert number
   if number.to_i <= 3000
     old_roman_numeral = ('M' * (number.to_i / 1000).to_i).to_s + ('D' * ((number.to_i%1000).to_i / 500).to_i).to_s + ('C' * ((number.to_i%500).to_i / 100).to_i).to_s + ('L' * ((number.to_i%100) / 50).to_i).to_s + ('X' * ((number.to_i%50).to_i / 10).to_i).to_s + ('V' * ((number.to_i%10).to_i / 5).to_i).to_s + ('I' * (number.to_i%5).to_i).to_s
-  else
-    puts 'That won\'t work. Please pick a number between 1 and 3000.'
   end
   puts old_roman_numeral
 end
